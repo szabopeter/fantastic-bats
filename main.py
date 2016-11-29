2a48524d1c1be14c10aa604d8db636db355d77c0
psz <psz@techtalk.at>
2016-11-29 12:34:22 +0100

    Fix style and the bug in removal during update.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!env python3
 
 import sys
-import math
+# import math
 
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
+
 def dbg(msg):
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
     print(msg, file=sys.stderr)
 
 # Grab Snaffles and try to throw them through the opponent's goal!
 # Move towards a Snaffle and use your team id to determine where you need to throw it.
+
 
 class P(object):
     def __init__(self, x, y):
@@ -38,11 +40,13 @@ MAX_THROW_POWER = 500
 TEAM_LTR = 0
 TEAM_RTL = 1
 
-def mkP(pt, team_id):
+
+def mkp(pt, team_id):
     if team_id == TEAM_LTR:
         return pt
     else:
         return P(MAPW-1-pt.x, pt.y)
+
 
 class Entity(object):
     def __init__(self, entity_id, entity_type, p, v, state):
@@ -50,37 +54,38 @@ class Entity(object):
         self.p, self.v, self.state = p, v, state
         self.markedForRemoval = False
 
+
 class GameState(object):
     def __init__(self, my_team_id):
         self.my_team_id = my_team_id
         self.entities = {}
     
-    def updateEntity(self, entity):
-        if not entity.entity_type in self.entities:
+    def update_entity(self, entity):
+        if entity.entity_type not in self.entities:
             self.entities[entity.entity_type] = {}
         
-        #if not entity.entity_id in self.entities[entity.entity_type]:
+        # if not entity.entity_id in self.entities[entity.entity_type]:
         self.entities[entity.entity_type][entity.entity_id] = entity
+        entity.markedForRemoval = False
         
-    def getMyWizard(self, entity_id):
+    def get_my_wizard(self, entity_id):
         return self.entities[ETYPE_WIZARD][entity_id]
         
-    def getTargetFor(self, wizard):
+    def get_target_for(self, wizard):
         if not self.entities[ETYPE_SNAFFLE]:
             return P(8000, 3750)
         
         return list(self.entities[ETYPE_SNAFFLE].values())[0].p
         
-    def getGoal(self):
-        if self.my_team_id == TEAM_LTR: return POLE_RIGHT
-        return POLE_LEFT
+    def get_goal(self):
+        return POLE_RIGHT if self.my_team_id == TEAM_LTR else POLE_LEFT
         
-    def markForRemoval(self):
+    def mark_for_removal(self):
         for entity_type in self.entities.keys():
             for entity in self.entities[entity_type].values():
                 entity.markedForRemoval = True
     
-    def removeMarkedOnes(self):
+    def remove_marked_entities(self):
         for entity_type in self.entities.keys():
             to_remove = []
             for entity in self.entities[entity_type].values():
@@ -90,17 +95,18 @@ class GameState(object):
                 del self.entities[entity_type][eid]
 
     def update(self, entities):
-        self.markForRemoval()
+        self.mark_for_removal()
         for entity in entities:
-            self.updateEntity(entity)
-        self.removeMarkedOnes()
+            self.update_entity(entity)
+        self.remove_marked_entities()
 
-    
+
 class GameLogic(object):
     def __init__(self):
         pass
-    
-    def execute(self):
+
+    @staticmethod
+    def execute():
         my_team_id = int(input())  # if 0 you need to score on the right of the map, if 1 you need to score on the left
         gamestate = GameState(my_team_id)
 
@@ -123,21 +129,21 @@ class GameLogic(object):
                 vx = int(vx)
                 vy = int(vy)
                 state = int(state)
-                entity = Entity(entity_id, entity_type, mkP(P(x, y), my_team_id), P(vx, vy), state)
+                entity = Entity(entity_id, entity_type, mkp(P(x, y), my_team_id), P(vx, vy), state)
                 to_update.append(entity)
 
             gamestate.update(to_update)
-            
+
             for i in range(2):
                 # Edit this line to indicate the action for each wizard (0 <= thrust <= 150, 0 <= power <= 500)
                 # i.e.: "MOVE x y thrust" or "THROW x y power"
-                wiz = gamestate.getMyWizard(i)
-                goal = gamestate.getGoal()
+                wiz = gamestate.get_my_wizard(i)
+                goal = gamestate.get_goal()
                 if wiz.state == STATE_WITH_SNAFFLE:
                     print("THROW %d %d 500"%(goal.x, goal.y,))
                 else:
                     #TODO: move to closest snaffle
-                    target = mkP(gamestate.getTargetFor(wiz), my_team_id)
+                    target = mkp(gamestate.get_target_for(wiz), my_team_id)
                     print("MOVE %d %d 100"%(target.x, target.y,))
 
 
